7831fd4904337cb01ad67e7f51eb70c160ea7495
lailaolabdev <lailaolabdev@gmail.com>
2025-04-03 09:33:13 +0700

    calculate driver distance

--- src/docs/calculation/index.ts
+++ src/docs/calculation/index.ts
@@ -99,3 +99,56 @@
  *                   type: string
  *                   example: "Error details here"
  */
+
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     BearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ *
+ * /v1/api/calculate/driver:
+ *   post:
+ *     summary: Calculate duration, distance, and price
+ *     description: Calculates duration, distance, and price based on specified attributes.
+ *     tags:
+ *       - Calculation
+ *     security:
+ *       - BearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               origin:
+ *                 type: string
+ *                 description: The origin for the start point.
+ *                 example: "17.9935451,102.5712961"
+ *               destination:
+ *                 type: string
+ *                 description: The destination for the end point.
+ *                 example: "17.971523,102.6200467"
+ *     responses:
+ *       200:
+ *         description: Calculation successful.
+ *       500:
+ *         description: Internal server error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 code:
+ *                   type: string
+ *                   example: "INTERNAL_SERVER_ERROR"
+ *                 message:
+ *                   type: string
+ *                   example: "Internal server error"
+ *                 detail:
+ *                   type: string
+ *                   example: "Error details here"
+ */
