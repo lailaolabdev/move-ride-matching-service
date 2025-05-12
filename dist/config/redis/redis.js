821501f1c386c6bff8346632b360aef5ff4391f9
lailaolabdev <lailaolabdev@gmail.com>
2025-05-12 13:47:43 +0700

    change redis option

--- dist/config/redis/redis.js
+++ dist/config/redis/redis.js
@@ -5,14 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.redis = void 0;
 const ioredis_1 = __importDefault(require("ioredis"));
-// Define the options with proper typing
-const options = {
-    port: 6379,
-    host: "localhost",
-    family: 4, // IPv4
-    db: 0,
-};
-// const options = {};
 // Initialize Redis client with options
 const redis = new ioredis_1.default({
     port: 6379,
--- src/config/redis/redis.ts
+++ src/config/redis/redis.ts
@@ -1,13 +1,4 @@
-import Redis, { RedisOptions } from "ioredis";
-
-// Define the options with proper typing
-const options: RedisOptions = {
-  port: 6379,
-  host: "localhost",
-  family: 4, // IPv4
-  db: 0,
-};
-// const options = {};
+import Redis from "ioredis";
 
 // Initialize Redis client with options
 const redis = new Redis({
