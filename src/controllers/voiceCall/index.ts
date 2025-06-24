4d989aa5da58a7523741c96fb9575c34267a90aa
lailaolabdev <lailaolabdev@gmail.com>
2025-06-24 16:59:07 +0700

    debug notification

--- src/controllers/voiceCall/index.ts
+++ src/controllers/voiceCall/index.ts
@@ -63,24 +63,17 @@ export const voiceCall = async (req: Request, res: Response) => {
       const dial = newtwiml.dial();
       dial.client(receiver);
 
-      console.log({
+      const body = {
         recipient: receiver,
         title: "Incoming Call",
         body: `Call from ${From}`,
         CallSid,
         From: caller,
         To: receiver
-      });
+      };
 
       if (CallStatus === 'ringing') {
-        const noti = await axios.post(`${process.env.NOTIFICATION_SERVICE_URL}/v1/api/notifications/voice-call`, {
-          "recipient": "68254ea9e2f2e47753d2e00b",
-          "title": "Incoming Call",
-          "body": "Call from ${From}",
-          "CallSid": "CAb1ab806bfbcc95abafd793d9b9fc02d4",
-          "From": "6853ba90575356cc7bb8def9",
-          "To": "68254ea9e2f2e47753d2e00b"
-        })
+        const noti = await axios.post(`${process.env.NOTIFICATION_SERVICE_URL}/v1/api/notifications/voice-call`, body)
 
         console.log(noti.data);
       }
