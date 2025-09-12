80a8e9ae63beab328a9e15ca3eecff4a27338b82
lailaolabdev <lailaolabdev@gmail.com>
2025-09-12 15:35:30 +0700

    uncomment redis

--- src/config/redis/redis.ts
+++ src/config/redis/redis.ts
@@ -1,14 +1,14 @@
 import Redis from "ioredis";
 
 // Initialize Redis client with options
-// const options = {
-//   port: 6379,
-//   host: process.env.REDIS_HOST,
-//   family: 4, // IPv4
-//   db: 0,
-// }
+const options = {
+  port: 6379,
+  host: process.env.REDIS_HOST,
+  family: 4, // IPv4
+  db: 0,
+}
 
-const options = {};
+// const options = {};
 
 const redis = new Redis(options)
 
