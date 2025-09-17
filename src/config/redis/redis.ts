dec0cd3ac372fafa08ec5cc3d181a75c2568906a
EljeThao <eljethao@gmail.com>
2025-09-17 11:28:02 +0700

    change redis

--- src/config/redis/redis.ts
+++ src/config/redis/redis.ts
@@ -1,14 +1,14 @@
 import Redis from "ioredis";
 
 // Initialize Redis client with options
-// const options = {
-//   port: 6379,
-//   host: "redis",
-//   family: 4, // IPv4
-//   db: 0,
-// }
+const options = {
+  port: 6379,
+  host: "redis",
+  family: 4, // IPv4
+  db: 0,
+}
 
-const options = {};
+// const options = {};
 
 const redis = new Redis(options)
 
