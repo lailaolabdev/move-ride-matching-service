82cff952d4a6071b08bb6bd6fa2a6fef75d15bd3
EljeThao <eljethao@gmail.com>
2025-09-19 00:04:11 +0700

    move redis to valkey instead of local

--- src/config/redis/redis.ts
+++ src/config/redis/redis.ts
@@ -1,12 +1,14 @@
-import Redis from "ioredis";
+import Redis, { RedisOptions } from "ioredis";
 
 // Initialize Redis client with options
-const options = {
+const options: RedisOptions = {
+  host: "valkey-move-uat-o50uly.serverless.apse1.cache.amazonaws.com", // no :6379 here
   port: 6379,
-  host: "redis",
   family: 4, // IPv4
   db: 0,
-}
+  tls: {}, // add this if you enabled in-transit encryption
+  // password: "YOUR_AUTH_TOKEN" // only if AUTH is enabled
+}; 
 
 // const options = {};
 
